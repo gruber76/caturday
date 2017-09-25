c04dcedba8376bee7e8465ea6a841cc33100b327
Joshua Gruber <joshua.gruber@gmail.com>
2017-09-25 15:55:17 -0600

    Use subdirectory for build

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -7,6 +7,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
+      require('karma-junit-reporter'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
@@ -22,7 +23,10 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev'
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'junit'],
+    junitReporter: {
+      outputDir: 'test-reports'
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
