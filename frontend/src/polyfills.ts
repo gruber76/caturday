7006e98d09e361f2e04a4e6aeb89cd6d18bd3b5f
Joshua Gruber <joshua.gruber@gmail.com>
2017-09-26 13:55:35 -0600

    Successfully using PhantomJS in a docker build that can be used in AWS CodeBuild. Fingers crossed.

--- frontend/src/polyfills.ts
+++ frontend/src/polyfills.ts
@@ -33,6 +33,11 @@
 // import 'core-js/es6/map';
 // import 'core-js/es6/weak-map';
 // import 'core-js/es6/set';
+import 'core-js/es6/symbol';
+import 'core-js/es6/object';
+import 'core-js/es6/string';
+import 'core-js/es6/array';
+
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
