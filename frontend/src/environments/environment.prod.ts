270519b17cff450a0f866c722178ee0074fdb442
Joshua Gruber <joshua.gruber@gmail.com>
2017-09-29 12:42:14 -0600

    Production environment vars

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,22 @@
 export const environment = {
-  production: true
-};
+  production: true,
+
+  region: 'us-east-2',
+  
+    identityPoolId: 'us-east-2:b7c9b51b-008d-4df2-92e5-d4fda68d8c55',
+    userPoolId: 'us-east-2_4w0hUHjlg',
+    clientId: '3najpish2vjn1im1ksm6otc6qg',
+  
+    rekognitionBucket: 'rekognition-pics',
+    albumName: 'usercontent',
+    bucketRegion: 'us-east-2',
+  
+    ddbTableName: 'LoginTrail',
+  
+    cognito_idp_endpoint: '',
+    cognito_identity_endpoint: '',
+    sts_endpoint: '',
+    dynamodb_endpoint: '',
+    s3_endpoint: ''
+  
+  };
